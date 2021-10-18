57edbc0bd6abd58483684ecace553cc19289ec3f
Tiago Barros <tbarros1337@hotmail.com>
2021-10-17 23:37:19 -0300

    feat: Adds update & delete category service

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -7,6 +7,18 @@ class CategoryController {
     res.json(categories);
   }
 
+  async show(req, res) {
+    const { id } = req.params;
+
+    const category = await CategoriesRepository.findById(id);
+
+    if (!category) {
+      return res.status(404).json({ error: 'Category not found' });
+    }
+
+    return res.json(category);
+  }
+
   async store(req, res) {
     const { name } = req.body;
 
@@ -17,6 +29,39 @@ class CategoryController {
     const category = await CategoriesRepository.create({ name });
 
     res.json(category);
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: 'name is required' });
+    }
+
+    const categoryAlreadyExists = await CategoriesRepository.findById(id);
+
+    if (!categoryAlreadyExists) {
+      return res.status(400).json({ error: 'category is required' });
+    }
+
+    const categoryName = await CategoriesRepository.findByName({ name });
+
+    if (name.toLocaleLowerCase() === categoryName?.toLocaleLowerCase()) {
+      return res.status(400).json({ error: 'This name already exists' });
+    }
+
+    const category = await CategoriesRepository.update(id, { name });
+
+    return res.json(category);
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    await CategoriesRepository.delete(id);
+
+    res.sendStatus(204);
   }
 }
 
