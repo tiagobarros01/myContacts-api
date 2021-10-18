13f17c181454da8f17cb5d7def3f99ac850758db
Tiago Barros <tbarros1337@hotmail.com>
2021-10-17 22:50:32 -0300

    feat: handlers errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+require('express-async-errors');
+
 const routes = require('./routes');
 
 const app = express();
@@ -7,5 +9,12 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use((error, req, res, next) => {
+  console.log('#### Error handler');
+  console.log(error);
+
+  res.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
