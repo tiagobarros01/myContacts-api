b4f8b9abf148c0549cf1c0510327aec458aaf909
Tiago Barros <tbarros1337@hotmail.com>
2021-12-20 16:59:46 -0300

    feat: Adds CORS to HTTP Methods & Headers

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,5 +1,17 @@
-module.exports = (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+const CORS_BASENAME = 'Access-Control-Allow-';
+
+const allowedMethods = {
+  Origin: 'http://localhost:3000',
+  Methods: '*',
+  Headers: '*',
+};
+
+module.exports = (_req, res, next) => {
+  for (const method in allowedMethods) {
+    if (Object.prototype.hasOwnProperty.call(allowedMethods, method)) {
+      res.setHeader(`${CORS_BASENAME}${method}`, allowedMethods[method]);
+    }
+  }
 
   next();
 };
