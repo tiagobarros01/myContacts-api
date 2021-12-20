5485376001767aa5eea88132c4cc177baa78bb46
Tiago Barros <tbarros1337@hotmail.com>
2021-12-20 16:02:03 -0300

    feat: Changes server port; Adds CORS Header

--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ require('express-async-errors');
 
 const routes = require('./routes');
 
+const SERVER_PORT = 3001;
+
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  next();
+});
 
 app.use(routes);
 
@@ -17,4 +25,4 @@ app.use((error, req, res, next) => {
   res.sendStatus(500);
 });
 
-app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
+app.listen(SERVER_PORT, () => console.log(`🔥 Server started at http://localhost:${SERVER_PORT}`));
