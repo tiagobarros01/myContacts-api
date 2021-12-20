f76cb378216b860768a8fd67f6b2ebf179a89f95
Tiago Barros <tbarros1337@hotmail.com>
2021-12-20 17:08:14 -0300

    feat: Adds cache to heards methods

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,15 +1,16 @@
-const CORS_BASENAME = 'Access-Control-Allow-';
+const CORS_BASENAME = 'Access-Control';
 
 const allowedMethods = {
-  Origin: 'http://localhost:3000',
-  Methods: '*',
-  Headers: '*',
+  'Allow-Origin': 'http://localhost:3000',
+  'Allow-Methods': '*',
+  'Allow-Headers': '*',
+  'Max-Age': '10',
 };
 
 module.exports = (_req, res, next) => {
   for (const method in allowedMethods) {
     if (Object.prototype.hasOwnProperty.call(allowedMethods, method)) {
-      res.setHeader(`${CORS_BASENAME}${method}`, allowedMethods[method]);
+      res.setHeader(`${CORS_BASENAME}-${method}`, allowedMethods[method]);
     }
   }
 
